f031158a75cb0f713a99a26173b995ba97eb49a0
Luke-Bradford-BBD <luke.bradford@projectolympus.co.za>
2024-05-08 18:32:12 +0200

    Fixed env var syntax error

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  host: process.env.env.DB_URL,
+  host: process.env.DB_URL,
   port: process.env.SERVER_PORT,
   database: process.env.DB_NAME,
   ssl: true,
