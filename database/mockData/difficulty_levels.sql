01492f03120b624dabf33731621ef032dea7455f
Luke-Bradford-BBD <luke.bradford@projectolympus.co.za>
2024-05-09 15:10:32 +0200

    Update difficulty_levels.sql

--- database/mockData/difficulty_levels.sql
+++ database/mockData/difficulty_levels.sql
@@ -4,14 +4,3 @@ INSERT INTO difficulty_levels (name, suits) VALUES
 ('Medium', 6),
 ('Hard', 8);
 --rollback DELETE FROM "difficulty_levels";
-
---changeset luke:dml:removeMockData:difficulty_levels
-DELETE FROM difficulty_levels;
---rollback DELETE FROM "difficulty_levels";
-
---changeset luke:dml:addNewMockData:difficulty_levels
-INSERT INTO difficulty_levels (name, suits) VALUES
-('Easy', 1),
-('Medium', 2),
-('Hard', 4);
---rollback DELETE FROM "difficulty_levels";
