fc6de78dc274fc685223834de7c5370ba0884f09
yashilBBD <yashil.luckan@bbd.co.za>
2024-05-09 13:06:52 +0200

    added server side OAuth 2

--- backend/app.js
+++ backend/app.js
@@ -50,12 +50,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// const usersRouter = require("./routes/users");
-// app.use("/users", usersRouter);
-//
-// const gameResultsRouter = require("./routes/gameResults");
-// app.use("/gameresults", gameResultsRouter);
-
 app.listen(port, () => {
   console.log(`Server running at http://127.0.0.1:${port}/`);
 });
