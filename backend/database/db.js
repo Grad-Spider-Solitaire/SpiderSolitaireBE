6911d2d8664d83bd731ff2b0387ef938f8656edd
Derryk Fivaz <Derryk.Fivaz@bbd.co.za>
2024-05-09 09:12:13 +0200

    db port var

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_URL,
-  port: process.env.SERVER_PORT,
+  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
 });
 
